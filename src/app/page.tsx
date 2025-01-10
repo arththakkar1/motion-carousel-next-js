3dacca05deadf9127cb62ae671605af3948a277d
Arth Thakkar <arththakkar@Arths-MacBook-Air.local>
2025-01-10 14:49:29 +0530

    Your commit message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,152 @@
-import Image from "next/image";
+"use client"
+import { MdNavigateNext,MdNavigateBefore } from "react-icons/md";
+import { motion,AnimatePresence, useMotionValue, useMotionValueEvent } from "motion/react";
+import { useEffect, useState } from "react";
+
+const img=["https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg","https://images.pexels.com/photos/210243/pexels-photo-210243.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/552785/pexels-photo-552785.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2","https://images.pexels.com/photos/1183021/pexels-photo-1183021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"]
+
+const DRAG_BUFFER=50;
+const SPRING={type:"spring",mass:3,stiffness:400,damping:50}
+const ONE_SECOUND=1000
+const AUTO_DELAY=ONE_SECOUND*10
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+const dragX=useMotionValue(0)
+const dragXProgess=useMotionValue(0)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+useMotionValueEvent(dragX,"change",(latest)=>{
+  if(typeof latest==="number"&&dragging){
+    dragXProgess.set(latest)
+  }else{
+    dragXProgess.set(0)
+  }
+})
+const [imgIndex,setImgIndex]=useState(0)
+const [dragging,setDragging]=useState(false)
+
+useEffect(()=>{
+  const intervalRef=setInterval(()=>{
+
+    const x=dragXProgess.get()
+
+    if(x===0){
+      setImgIndex((prev)=>{
+        if(prev===img.length-1){
+          return 0
+        }
+        return prev + 1;
+      })
+    }
+
+  },AUTO_DELAY)
+
+  return ()=>clearInterval(intervalRef)
+
+},[])
+
+const onDrageStart = () =>{
+  setDragging(true)
+}
+
+const handlePrev=()=>{
+  if(imgIndex>0){
+    setImgIndex((prev)=>prev-1)
+  }
+}
+
+const handleNext=()=>{
+  if(imgIndex<img.length-1){
+    setImgIndex((prev)=>prev+1)
+  }
+}
+
+const onDrageEnd = () =>{
+  setDragging(false)
+
+  const x=dragX.get()
+
+
+  if(x<=-DRAG_BUFFER&&imgIndex<img.length-1){
+    setImgIndex((prev)=>prev+1)
+  }else if(x>=DRAG_BUFFER&&imgIndex>0){
+    setImgIndex((prev)=>prev-1)
+  }
+}
+
+  return (
+  <>
+    <AnimatePresence>
+      <div className="relative min-h-screen overflow-hidden bg-neutral-950 py-8">
+        <motion.div
+        drag="x"
+        dragConstraints={
+          {
+            right:0,
+            left:0
+          }
+        }
+        transition={SPRING}
+        style={{
+          x:dragX
+        }}
+        onDragStart={onDrageStart}
+        onDragEnd={onDrageEnd}
+        animate={{
+          translateX:`-${imgIndex*100}%`,
+        }}
+        className="curser-grab active:cursor-grabbing flex items-center">
+          <Images imgIndex={imgIndex} handlePrev={handlePrev} handleNext={handleNext} />
+        </motion.div>
+        <GradientEdges/>
+      </div>
+    </AnimatePresence>
+  </>
+  )
+  
+}
+
+const Images = ({imgIndex,handlePrev,handleNext}:{imgIndex:number,handlePrev:any,handleNext:any}) => {
+  return (
+    <>
+      {img.map((src, index) => (
+        <motion.div 
+        key={index} 
+        style={{
+        backgroundImage: `url(${src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        }}
+        animate={{
+          scale:imgIndex===index?0.95:0.85
+        }}
+        transition={SPRING}
+        className="aspect-video w-screen shrink-0 rounded-xl bg-neutral-800 object-cover">
+
+          <div className="flex justify-between h-full items-center">
+          <button onClick={handlePrev}>
+            <MdNavigateBefore className={`text-5xl rounded-lg ${
+              imgIndex===0?"hidden":"block"
+            }`}/>
+          </button>
+          <button onClick={handleNext}>
+            <MdNavigateNext className={`text-5xl rounded-lg ${
+              imgIndex<img.length-1?"block":"hidden"
+            }`}/>
+          </button>
+          </div>
+        </motion.div>
+      ))}
+    </>
   );
+}
+
+const GradientEdges=()=>{
+  return(
+  <>
+    <div className="pointer-events-none absolute bottom-0 left-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-r from-neutral-950/50 to-neutral-950/0">
+    
+    </div>
+    <div className="pointer-events-none absolute bottom-0 right-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-l from-neutral-950/50 to-neutral-950/0"/>
+  </>
+);
 }
